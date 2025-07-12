60505e24794df0719ebff22b395d89d58f25512e
Gonzalo Damian Atadia <gonzaloatadia@gmail.com>
2025-07-12 14:26:10 -0300

    listo a entregar

---
+++ js/contacto.js
@@ -0,0 +1,6 @@
+let nombre = prompt("¿Cuál es tu nombre?");
+if (nombre) {
+  let saludo = "Hola, " + nombre.toUpperCase() + "!. Por favor rellene el formulario y a la brevedad le contestaremos";
+  alert(saludo);
+  console.log("El usuario se llama " + nombre);}
+  
